a72e4825f067eebdf7b7783b6fc9c3a162ae2bfc
jkwaser <jkwaser@gmail.com>
2022-04-17 23:53:42 +1000

    Added notes for tomorrow outlining the scope of the problem ahead, css/js into buttons is all easy, the hard part is yet to be solved.

--- calculator.js
+++ calculator.js
@@ -1,20 +1,20 @@
-function adder(a,b) {
+function adder(a,b) { // Adds numbers
 return a + b;
 }
 
-function subtractor(a,b) {
+function subtractor(a,b) { // Subtracts numbers
     return a - b;
 }
 
-function multiplier(a,b) {
+function multiplier(a,b) { // Multiplies numbers
     return a * b;
 }
 
-function divider(a,b) {
+function divider(a,b) { // Divides numbers
     return a / b;
 }
 
-function operate(a,b, operator) {
+function operate(a,b, operator) { // Takes in an operator and returns the two numbers with that operator applies
     if (operator == "+") {
         return adder(a,b);
     }
@@ -27,5 +27,17 @@ function operate(a,b, operator) {
     if (operator == "/") {
         return divider(a,b);
     }
-    else (return "Incorrect operator!")
+    else {return "Incorrect operator!"} // Can only accept the four basic operations. Should not be an issue as operands are input from buttons, this is here as a failsafe.
 }
+
+/*
+Temporary notes to help figure this out. There are basically two cases.
+Case one is easy - I get a number, an operator, a second number, and the equals sign.
+Just evaluate as per the operate function and store the result as the first number.
+Case two is trickier - I get a number, and operator, a second number, and a second operator.
+I then need to evaluate the number/operator/number triad, use it as the new first number,
+and add the new operator to await the second number.
+
+If I can resolve both of those cases functionally I can hook them up to the 
+buttons and the rest is pretty straightforward.
+*/
